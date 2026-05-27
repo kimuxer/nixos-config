7b66f426e66868099347a70cb4e324789778c493
kimuxer <604719@qq.com>
2026-05-27 21:12:22 +0800

    change limine.nix

--- system/boot.nix
+++ system/boot.nix
@@ -26,16 +26,17 @@
       enable = true;
       efiSupport = true;
       enableEditor = true;
+      maxGenerations = 10;
       style = {
         interface.resolution = "1920x1080";
-        wallpapers = [ ];
-        backdrop = "1E1E2E";
-        graphicalTerminal.background = "1E1E2E";
-        graphicalTerminal.foreground = "CDD6F4";
-        graphicalTerminal.brightBackground = "585B70";
-        graphicalTerminal.brightForeground = "CDD6F4";
-        graphicalTerminal.palette = "1e1e2e;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4";
-        graphicalTerminal.brightPalette= "585b70;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4";
+      #  wallpapers = [ ];
+      #  backdrop = "1E1E2E";
+      #  graphicalTerminal.background = "1E1E2E";
+      #  graphicalTerminal.foreground = "CDD6F4";
+      #  graphicalTerminal.brightBackground = "585B70";
+      #  graphicalTerminal.brightForeground = "CDD6F4";
+      #  graphicalTerminal.palette = "1e1e2e;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4";
+      #  graphicalTerminal.brightPalette= "585b70;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4";
       };
 
       extraEntries = "
