c3886acc6960f99b572718971ee2e1d2d0c0812e
kimuxer <604719@qq.com>
2026-05-28 22:31:01 +0800

    locale = zh_CN.UTF-8

--- system/hardware/input.nix
+++ system/hardware/input.nix
@@ -1,9 +1,9 @@
-{ config, pkgs, ... }: {
-  i18n.defaultLocale = "en_US.UTF-8";
+{ pkgs, ... }: {
+  #i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "zh_CN.UTF-8";
   i18n.extraLocaleSettings = {
     LC_CTYPE = "zh_CN.UTF-8";
   };
-  #i18n.defaultLocale = "zh_CN.UTF-8";
   i18n.inputMethod = {
     enable = true; 
     type = "fcitx5"; 
