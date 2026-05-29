e9e1a9b5e999e1616ce89466a64a791edfabad72
kimuxer <604719@qq.com>
2026-05-29 22:25:52 +0800

    使用kanata映射右alt键为mod+shift

--- modules/system/kanata.nix
+++ modules/system/kanata.nix
@@ -1,21 +1,32 @@
-{ pkgs, config, ... }:
+{ ... }:
 {
   services.kanata = {
     enable = true;
     keyboards.default = {
       config = ''
-        ;; 只点名 Caps Lock 这一个键
         (defsrc
-          caps)
+          caps ralt 1 2 3 4 5 6 7 8 9)
 
         (defalias
-          ;; 核心修复：将 capslock 改为 caps
-          ;; 单击变大写，长按变 Super (LMet)
+          ;; 你原本的配置：单点 Caps，长按 Super (Meta)
           cap (tap-hold-press 150 150 caps lmet)
+          
+          ;; 新增：按住 ralt 进入 move_layer 层
+          mv_lyr (layer-while-held move_layer)
         )
 
         (deflayer default
-          @cap)
+          ;; @cap 对应 Caps 逻辑
+          ;; @mv_lyr 对应 ralt 逻辑
+          @cap @mv_lyr 1 2 3 4 5 6 7 8 9)
+
+        (deflayer move_layer
+          ;; 这里的 _ 表示保持上层的按键状态 (即按住 ralt)
+          ;; _ (对应 caps)
+          _
+          ;; M-S-1 代表 Super(Meta) + Shift + 1
+          ;; 这里的 M 必须和你在 Niri 里的 Mod 定义一致
+          M-S-1 M-S-2 M-S-3 M-S-4 M-S-5 M-S-6 M-S-7 M-S-8 M-S-9)
       '';
     };
   };
