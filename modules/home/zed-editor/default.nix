4d6371e080d934ca199388ce8cdf41784e8a426d
kimuxer <604719@qq.com>
2026-05-31 18:37:04 +0800

    修复zed 配置

--- modules/home/zed-editor/default.nix
+++ modules/home/zed-editor/default.nix
@@ -17,7 +17,7 @@
       buffer_font_family = "JetBrainsMono Nerd Font"; # 使用你系统已全局安装的代码字体
       buffer_font_size = 12;                         # 代码字体大小
       buffer_font_weight = 400;                      # 字体粗细
-      line_height = "comfortable";                   # 舒适的行高
+      buffer_line_height = "comfortable";                   # 舒适的行高
 
       # --- 2. 编辑器界面 UI 字体 ---
       ui_font_family = "Inter";                      # 使用你系统已全局安装的 UI 英文首选字体
@@ -27,12 +27,13 @@
       terminal = {
         font_family = "JetBrainsMono Nerd Font";
         font_size = 12;
+        line_height = "standard";
       };
 
       # --- 4. 其他推荐体验参数 ---
-      theme = "One Dark";                            # 主题名称（可按喜好修改）
-      relative_line_numbers = true;                  # 相对行号（匹配你 NVF 编辑器的阅读习惯）
-      vim_mode = false;                              # 是否开启 Vim 快捷键模式（按需改为 true）
+      theme = "One Dark";                                         # 主题名称（可按喜好修改）
+      relative_line_numbers = "enabled";                 # 相对行号（匹配你 NVF 编辑器的阅读习惯）
+      vim_mode = false;                                             # 是否开启 Vim 快捷键模式（按需改为 true）
 
       # 缩进参考线
       indent_guides = {
