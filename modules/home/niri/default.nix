84961b2404ccee683d06f047e9a52fbb5231d213
Kim <4003419+kimuxer@users.noreply.github.com>
2026-06-17 16:18:05 +0800

    优化 Niri 架构：主控 config.kdl 顶层外挂，子配置收拢至 configs 文件夹

--- modules/home/niri/default.nix
+++ modules/home/niri/default.nix
@@ -1,5 +1,33 @@
+# modules/home/niri/default.nix
 { ... }:
-
 {
-  home.file.".config/niri".source = ./configs;
+  home.file = {
+    ".config/niri/config.kdl".text = ''
+      // ======================
+      //   niri : config.kdl
+      // ======================
+
+      include "configs/animations.kdl"
+      include "configs/binds.kdl"
+      include "configs/cursor.kdl"
+      include "configs/environment.kdl"
+      include "configs/input.kdl"
+      include "configs/layer-rules.kdl"
+      include "configs/layout.kdl"
+      include "configs/output.kdl"
+      include "configs/window-rules.kdl"
+
+      prefer-no-csd
+      hotkey-overlay { skip-at-startup; }
+
+      spawn-at-startup {
+        run "fcitx5" "-d" "--replace"
+        run "noctalia"
+      }
+    '';
+    # Copy configs to ~/.config/niri/configs
+    ".config/niri/configs" = {
+      source = ./configs;
+    };
+  };
 }
