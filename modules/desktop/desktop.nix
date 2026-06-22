3e8bfd0c3de8c4ebffc38624fd06af6c10e1889a
Kim <4003419+kimuxer@users.noreply.github.com>
2026-06-22 16:19:15 +0800

    fix(desktop): 添加 xdg.portal.xdgOpenUsePortal = true

--- modules/desktop/desktop.nix
+++ modules/desktop/desktop.nix
@@ -24,6 +24,7 @@
   # ============================
   xdg.portal = {
     enable = true;
+    xdgOpenUsePortal = true;
     extraPortals = [
       pkgs.xdg-desktop-portal-gnome
       pkgs.xdg-desktop-portal-gtk
