8b1e1f44fa0461ef6ea4c0baf28ee59a6ce5f865
Kim <4003419+kimuxer@users.noreply.github.com>
2026-06-23 15:48:34 +0800

    feat(noctalia): 添加hooks

--- modules/home/noctalia/default.nix
+++ modules/home/noctalia/default.nix
@@ -11,5 +11,8 @@
       shell.launch_apps_as_systemd_services = true;
       location.address = "Dalian, CN";
     };
+    hooks = {
+      wallpaper_changed = [ "noctalia:greeter-sync" ];
+    };
   };
 }
