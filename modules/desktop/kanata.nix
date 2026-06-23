f3448ebe5c1de45162a1bf321896686cc81704b7
Kim <4003419+kimuxer@users.noreply.github.com>
2026-06-23 22:10:09 +0800

    fix(kanata): 修复kanata

--- modules/desktop/kanata.nix
+++ modules/desktop/kanata.nix
@@ -7,15 +7,12 @@
       config = ''
         (defsrc
           caps ralt 1 2 3 4 5 6 7 8 9 h j k l q)
-
         (defalias
           caps (tap-hold-press 120 150 caps lmet)
           mv_lyr (layer-while-held move_layer)
         )
-
         (deflayer default
           @caps @mv_lyr 1 2 3 4 5 6 7 8 9 h j k l q)
-
         (deflayer move_layer
           _ _
           M-S-1 M-S-2 M-S-3 M-S-4 M-S-5 M-S-6 M-S-7 M-S-8 M-S-9
